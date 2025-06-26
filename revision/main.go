d07cc25c84cda9cdfddcec34fb28345ac598eb3e
Arbind Das <dasarbind269@gmail.com>
2025-06-26 12:46:48 +0545

    Done

--- revision/main.go
+++ revision/main.go
@@ -1,95 +1,106 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Address struct {
-	city     string
-	province string
+// add two number using function
+// without return anything void function
+func addTwoNumber(num1, num2 int) {
+	ans := num1 + num2
+	fmt.Printf("the addition of %d and %d is : %d\n", num1, num2, ans)
 }
 
-type Student struct {
-	id    int64
-	name  string
-	class string
-
-	Address Address
-}
-
-func printUserName(user string) {
-	fmt.Println(user)
-}
-
-func printUserNameTwo(user string) string {
-	return user
-}
-
-func addTwoNumbers(num1, num2 float64) {
-	fmt.Println(num1 + num2)
-}
-
-func addTwoNumbersTwo(num1, num2 float64) float64 {
+// add two number with return value
+func addTwoNumberWithReturns(num1, num2 int) int {
 
 	return num1 + num2
 }
 
-func print1toNnumbers(num int64) int64 {
-	ans := 0
+func addTwoNumberWithPointer(num1, num2 *int) {
 
-	for i := 1; i <= int(num); i++ {
-		ans += i
-	}
-
-	return int64(ans)
+	ans := *num1 + *num2
+	fmt.Println(ans)
 }
 
-func print(num int64) {
+func addTwoNumberWithPointerWithReturns(num1, num2 *int) int {
 
-	i := 1
-	for i <= int(num) {
-		fmt.Println(i)
-		i++
+	return *num1 + *num2
+}
+
+func printProductsData(products []int) {
+
+	fmt.Println("With index and both values")
+	for i, val := range products {
+		fmt.Printf("Index %d , values %d\n", i, val)
+	}
+
+	fmt.Println("with values only")
+
+	for _, values := range products {
+		fmt.Printf("values %d\n", values)
 	}
 }
 func main() {
-
-	structRes := Student{
-		1,
-		"Arbind",
-		"ten",
-		Address{
-			city:     "Kathmandu",
-			province: "lumbani",
-		},
-	}
-	fmt.Println(structRes)
-
-	print(10)
-
-	nat := print1toNnumbers(5)
-	fmt.Println(nat)
-
-	res := addTwoNumbersTwo(90, 9)
-	fmt.Println(res)
-
-	addTwoNumbers(10, 19)
-
-	var num1, num2 float64
-
-	fmt.Println("Enter the first number")
-	fmt.Scanln(&num1)
-	fmt.Println("Enter the second number")
-	fmt.Scanln(&num2)
-
-	sum := num1 + num2
-
-	// fmt.Println("the sum of %.2f and %.2f is %.2f \n", num1, num2, sum) // not work with println
-	fmt.Printf("The sum of %.2f and %.2f is: %.2f\n", num1, num2, sum)
 	fmt.Println("jai shree ram")
 
-	printUserName("Arbind Das")
+	addTwoNumber(10, 99)
 
-	ans := printUserNameTwo("aryan kumar das")
+	fmt.Println(addTwoNumberWithReturns(90, 90))
+
+	num1 := 100
+	num2 := 100
+
+	addTwoNumberWithPointer(&num1, &num2)
+
+	fmt.Println(addTwoNumberWithPointerWithReturns(&num1, &num2))
+
+	//The & operator means: get the memory address of the struct.
+	// * works with the value at that address.
+
+	numbers := []int{10, 10, 10, 20}
+
+	// fmt.Println(numbers)
+
+	// for i, val := range numbers {
+	// 	fmt.Printf("index %d  values %d\n", i, val)
+	// }
+
+	printProductsData(numbers)
+
+	ans := map[string]int{
+		"Alice": 25,
+		"Bob":   30,
+		"Carol": 22,
+	}
+
 	fmt.Println(ans)
+	// Accessing a value
+	fmt.Printf("the age of ALice is : %d\n", ans["Alice"])
+
+	// Adding a new key-value pair
+	ans["Aryan"] = 32
+
+	// Updating a value
+	ans["Alice"] = 90
+
+	// Deleting a key
+	delete(ans, "Bob")
+
+	fmt.Println(ans)
+
+	// Looping through the map
+
+	for name, age := range ans {
+		fmt.Printf("%s is %d years old\n", name, age)
+
+	}
+
+	// Check if a key exists
+	age, exists := ans["Alice"]
+
+	if exists {
+		fmt.Printf("Alice age  is %d\n", age)
+	} else {
+		fmt.Printf("ALice is not present in the map")
+	}
+
 }
