462fdb37ac43931f560abc515dc9bf7d3c1f205a
Arbind Das <dasarbind269@gmail.com>
2025-06-28 09:50:56 +0545

    Done

--- revisionTime/main.go
+++ revisionTime/main.go
@@ -2,46 +2,39 @@ package main
 
 import "fmt"
 
-type Addition struct {
+// AddValues holds two float64 numbers to perform addition
+type AddValues struct {
 	value1 float64
 	value2 float64
 }
 
-func initializeValues(num1, num2 float64) *Addition {
-
-	return &Addition{value1: num1, value2: num2}
+// NewAddValues is a constructor that initializes and returns a pointer to AddValues
+func NewAddValues(num1, num2 float64) *AddValues {
+	return &AddValues{value1: num1, value2: num2}
 }
 
-func (a *Addition) addTwo() {
-	ans := a.value1 + a.value2
-	fmt.Println(ans)
+// Sum returns the addition of the two float64 values
+// public function
+func (a *AddValues) Sum() float64 {
+	return a.value1 + a.value2
 }
-func addTwoNumberWithUsingPointers(num1, num2 *int) {
 
-	ans := *num1 + *num2
-
-	fmt.Printf("the addition of %d + %d is  = %d\n ", *num1, *num2, ans)
+// addIntPointers adds two integers via pointers and returns the result
+// private function
+func addIntPointers(num1, num2 *int) int {
+	return *num1 + *num2
 }
+
 func main() {
-	// & gives you the Memory address of variable (a pointer)
-	// * Takes a pointer (memory address) and gives you the actual value stored at the address (dereference the pointer)
+	num1 := 100
+	num2 := 100
 
-	number1 := 100
-	number2 := 100
-
-	addTwoNumberWithUsingPointers(&number1, &number2)
+	result := addIntPointers(&num1, &num2)
+	fmt.Printf("The addition of %d + %d = %d\n", num1, num2, result)
 
 	fmt.Println("Jai Shree Ram")
 
-	ans := initializeValues(10, 10)
-	ans.addTwo()
-	fmt.Println(ans)
-
-	result := Addition{
-		value1: 100,
-
-		value2: 100,
-	}
-
-	fmt.Println(result)
+	addVals := NewAddValues(10.0, 10.0)
+	sum := addVals.Sum()
+	fmt.Printf("The sum of %.2f and %.2f = %.2f\n", addVals.value1, addVals.value2, sum)
 }
